4acd2707250e36fbbf16e239558bae7fc8870b51
Pkay <p.kazemekaitis@gmail.com>
2019-03-28 08:11:46 +0200

    Initial Heroku commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from './header/header.component';
+import { BannerComponent } from './banner/banner.component';
+import { BurgerButtonComponent } from './header/burger-button/burger-button.component';
+import { SideDrawerComponent } from './header/side-drawer/side-drawer.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BannerComponent,
+    BurgerButtonComponent,
+    SideDrawerComponent,
+ 
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
